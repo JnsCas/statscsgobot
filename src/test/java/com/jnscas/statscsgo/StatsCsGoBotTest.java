67c0979748ca9370e0334571d7bd4644e8d5d917
JnsCas <jonas.casteglione@despegar.com>
2020-01-16 22:36:19 -0300

    settings for release

--- src/test/java/com/jnscas/statscsgo/StatsCsGoBotTest.java
+++ src/test/java/com/jnscas/statscsgo/StatsCsGoBotTest.java
@@ -12,7 +12,6 @@ import org.telegram.telegrambots.meta.api.objects.User;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
